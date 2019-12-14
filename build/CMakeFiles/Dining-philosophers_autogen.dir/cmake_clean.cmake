5377341a3ba9d48dfe4e0b29f5596cc29958a96d
Francesco Argentieri <francesco@MBPdiFrancesco.homenet.telecomitalia.it>
2019-12-14 10:06:11 +0100

    del: build files

--- build/CMakeFiles/Dining-philosophers_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Dining-philosophers_autogen"
-  "Dining-philosophers_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Dining-philosophers_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
