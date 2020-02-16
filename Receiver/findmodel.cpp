c3e8523944acd13d4bd0fbef18f72944bac53d9e
Francesco <francesco.argentieri89@gmail.com>
2020-02-16 08:23:19 +0100

    add: find model class

--- Receiver/findmodel.cpp
+++ Receiver/findmodel.cpp
@@ -1,6 +1,85 @@
 #include "findmodel.hpp"
 
-FindModel::FindModel(QObject *parent) : QObject(parent)
-{
+#include <QDebug>
+#include <QDir>
+#include <QDirIterator>
+#include <QVBoxLayout>
+#include <QWidget>
 
+#include "log/logger.h"
+
+FindModel::FindModel(QString path, QDialog* parent)
+    : QDialog(parent), m_currentdir(path) {
+  auto vert_layout = new QVBoxLayout;
+  m_combo = new QComboBox(this);
+
+  vert_layout->addWidget(m_combo);
+  this->setLayout(vert_layout);
+  this->resize(100, 45);
+
+  m_combo->setCurrentIndex(-1);
+
+  connect(m_combo, &QComboBox::currentTextChanged, this,
+          &FindModel::updateModel);
+  index = -1;
+  find();
+}
+
+FindModel::~FindModel() {
+  //  if (counter_file) {
+  //    delete counter_file;
+  //    counter_file = nullptr;
+  //  }
+}
+
+void FindModel::updateIndex(int i) {
+  if (i != index) {
+    index = i;
+    qDebug() << i;
+  }
+}
+
+void FindModel::updateModel(const QString& t) {
+#if LOGGER_UI
+  LOG(DEBUG, "return model")
+  qDebug() << "value selected " << t;
+  qDebug() << "associated model "
+           << m_list_model.
+#endif
+              model_name = t;
+}
+
+int FindModel::count() { return m_list_model.size(); }
+
+QString FindModel::getModelPath() {
+#if LOGGER_UI
+  LOG(DEBUG, "return model")
+  qDebug() << "value selected " << index;
+  qDebug() << "associated model " << m_list_model.contains(model_name);
+#endif
+  return m_list_model.at(index);
+}
+
+void FindModel::find() {
+  auto dir = QDir(m_currentdir);
+  QStringList filter;
+  filter << QString("*.tflite");
+  dir.setFilter(QDir::Files);
+  QFileInfoList entries = dir.entryInfoList();
+  for (auto it = entries.begin(); it != entries.end(); ++it) {
+    m_list_model << it->absoluteFilePath();
+    m_list_model.sort();
+    m_combo->addItem(it->completeBaseName());
+  }
+  switch (entries.size()) {
+    case 0: {
+      qDebug() << "error no model";
+      std::abort();
+    }
+    case 1:
+      m_combo->setCurrentIndex(0);
+      break;
+    default:
+      m_combo->setCurrentIndex(-1);
+  }
 }
--- Receiver/findmodel.hpp
+++ Receiver/findmodel.hpp
@@ -1,16 +1,32 @@
 #ifndef FINDMODEL_HPP
 #define FINDMODEL_HPP
 
-#include <QObject>
+#include <QComboBox>
+#include <QDialog>
+#include <QProgressDialog>
 
-class FindModel : public QObject
-{
+class FindModel : public QDialog {
   Q_OBJECT
  public:
-  explicit FindModel(QObject *parent = nullptr);
+  explicit FindModel(QString path, QDialog *parent = nullptr);
+  ~FindModel();
 
+  int count();
+  QString getModelPath();
+ private slots:
+
+  void updateIndex(int i);
+  void updateModel(const QString &t);
  signals:
 
+ private:
+  void find();
+  QComboBox *m_combo{nullptr};
+  QString m_currentdir;
+  QStringList m_list_model;
+  QString model_name;
+  int counter_file;
+  int index;
 };
 
 #endif // FINDMODEL_HPP
