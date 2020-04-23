456053ded7afaff00b3973af327b858276d7cf01
Francesco <francesco.argentieri89@gmail.com>
2020-04-23 17:37:33 +0200

    fix: correction index
    increment of 1

--- Receiver/tf/object_detection.cpp
+++ Receiver/tf/object_detection.cpp
@@ -12,17 +12,15 @@ void ObjectDetection::SearchObject(const std::vector<TfLiteTensor *> &outputs,
     detection_classes_  = std::make_unique<float>(*TensorData<float>(outputs[1], 0));
     detection_scores_   = std::make_unique<float>(*TensorData<float>(outputs[2], 0));
     num_detections_     = static_cast<int>(*TensorData<float>(outputs[3], 0));
-
-    for (int i = 0; i < num_detections_; i++) {
-      // get class
-      int cls = static_cast<int>(detection_classes_.get()[i]);
-      if (cls == 0 || cls <= max_index_class) continue;
+    // extract class and score, if valid append to list of result
+    for (int i = 0; i < num_detections_; ++i) {
+      int cls = static_cast<int>(detection_classes_.get()[i] + 1);
+      if (cls == 0) continue;
       auto score = static_cast<float>(detection_scores_.get()[i]);
       if (score < threshold || score <= 1.00f) {
         LOG(LevelAlert::W, "low score: ", score, ", class ", cls)
         break;
       }
-
       // get corners coordinates
       const auto top =
           static_cast<qreal>(detection_boxes_.get()[4 * i] * img.height());
