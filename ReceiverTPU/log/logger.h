d5a52ad49cdaa5cfdb84162203c5631bd0f464c9
Francesco <francesco.argentieri89@gmail.com>
2020-04-26 09:54:02 +0200

    add: log function

--- ReceiverTPU/log/logger.h
+++ ReceiverTPU/log/logger.h
@@ -1,94 +1,110 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include <chrono>
+#include <array>
 #include <iostream>
-#include <ratio>
+#include <ostream>
+#include <string>
+#include <type_traits>
 
-#if __cplusplus
-extern "C" {
-#endif
+template <typename E>
+constexpr auto to_underlying(E e) noexcept {
+  return static_cast<std::underlying_type_t<E>>(e);
+}
 
-#include <stdarg.h>
-#include <stdio.h>
-
-#ifndef LOGGER
-#define LOGGER 1
+#define LOGGER 0
+#if LOGGER
 #define LOGGER_GEN 1
 #define LOGGER_UI 1
-#endif  // LOGGER
-
-#if LOGGER_UI
-#define LOG(LEVEL, ...) logger(LEVEL, __FILE__, __LINE__, __VA_ARGS__);
-#else
-#define LOG(LEVEL, ...)
-#endif
-
-#if LOGGER_GEN
+#define LOGGER_CNN 1
 #define LOG(LEVEL, ...) logger(LEVEL, __FILE__, __LINE__, __VA_ARGS__);
 #else
 #define LOG(LEVEL, ...)
 #endif
 
 // define constant color hex
-extern const char RED[];
-extern const char GREEN[];
-extern const char YELLOW[];
-extern const char BLUE[];
-extern const char MAGENTA[];
-extern const char CYAN[];
-extern const char GREY[];
-extern const char LIGHT_GREY[];
-extern const char LIGHT_RED[];
-extern const char LIGHT_GREEN[];
-extern const char LIGHT_YELLOW[];
-extern const char LIGHT_BLUE[];
-extern const char LIGHT_MAGENTA[];
-extern const char LIGHT_CYAN[];
-extern const char WHITE[];
-extern const char RESET[];
+extern const std::string RED;
+extern const std::string GREEN;
+extern const std::string YELLOW;
+extern const std::string BLUE;
+extern const std::string MAGENTA;
+extern const std::string CYAN;
+extern const std::string GREY;
+extern const std::string LIGHT_GREY;
+extern const std::string LIGHT_RED;
+extern const std::string LIGHT_GREEN;
+extern const std::string LIGHT_YELLOW;
+extern const std::string LIGHT_BLUE;
+extern const std::string LIGHT_MAGENTA;
+extern const std::string LIGHT_CYAN;
+extern const std::string WHITE;
+extern const std::string RESET;
 
-// define level's name
-extern const char *levels_name[];
+enum class LevelAlert : int { I, T, D, W, E, F };
 
-// define level's color
-extern const char *levels_color[];
-
-typedef enum level { TRACE, DEBUG, INFO, WARN, ERROR, FATAL } level_t;
-
-/*
- */
-extern void logger(level_t level, const char *file, int line, const char *fmt,
-                   ...);
-
-#if __cplusplus
+inline std::string get_level(LevelAlert lvl) {
+  std::string alert;
+  switch (lvl) {
+    case LevelAlert::I:
+      alert = "I ";
+      break;
+    case LevelAlert::T:
+      alert = "T ";
+      break;
+    case LevelAlert::D:
+      alert = "D ";
+      break;
+    case LevelAlert::W:
+      alert = "W ";
+      break;
+    case LevelAlert::E:
+      alert = "E ";
+      break;
+    case LevelAlert::F:
+      alert = "F ";
+      break;
+  }
+  return alert;
 }
-#endif
 
-class TimeMeter {
-public:
-  explicit TimeMeter() {}
-  inline void start();
-  inline void stop();
-  void getPartialElapsed();
-  void getTotalElapsed();
-  ~TimeMeter();
+inline std::ostream &operator<<(std::ostream &os, LevelAlert lvl) {
+  auto alert = get_level(lvl);
+  os << alert;
+  return os;
+}
 
-private:
-  std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
-  std::chrono::time_point<std::chrono::high_resolution_clock> m_stop;
-  std::chrono::duration<double, std::micro> m_partial_elapsed;
-  std::chrono::duration<double, std::micro> m_total_elapsed;
-};
+inline const std::string &color_out(LevelAlert lvl) {
+  switch (lvl) {
+    case LevelAlert::I:
+      return GREEN;
 
-class StopWatch {
- public:
-  explicit StopWatch();
-  ~StopWatch();
+    case LevelAlert::T:
+      return CYAN;
 
- private:
-  std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
-  std::chrono::time_point<std::chrono::high_resolution_clock> m_stop;
-};
+    case LevelAlert::D:
+      return CYAN;
 
-#endif // LOGGER_H
+    case LevelAlert::W:
+      return LIGHT_RED;
+
+    case LevelAlert::E:
+      return RED;
+
+    case LevelAlert::F:
+      return MAGENTA;
+
+    default:
+      return RESET;
+  }
+}
+
+template <typename... T>
+inline void logger(LevelAlert lvl, const char *file, int line, T &&... args) {
+  std::cerr << color_out(lvl) << lvl << RESET << " " << file << " l: " << line
+            << "] ";
+  int expander[]{0, (void(std::cerr << std::forward<T>(args) << " "), 0)...};
+  static_cast<void>(expander);
+  std::cerr << std::endl;
+}
+
+#endif  // LOGGER_H
